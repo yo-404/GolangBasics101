2805cf1bc95a8cff7da8ae5691e2aaac0d689b59
yo-404 <yogeshy2801@gmail.com>
2023-08-19 20:21:52 +0530

    time handling

---
+++ 05timehandling/go.mod
@@ -0,0 +1,3 @@
+module github.com/yo-404/GolangBasics101/05timehandling
+
+go 1.21.0
---
+++ 05timehandling/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// after sleep and tick
+	fmt.Println("Welcome to time handling in golang")
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006"))
+	fmt.Println(presentTime.Format("Jan 02 2006"))
+	fmt.Println(presentTime.Format("Jan 02 2006 IST 15:04:05"))
+	fmt.Println(presentTime.Format("Monday Jan 02 2006 IST 15:04:05"))
+
+	createDate := time.Date(2021, time.December, 10, 20, 18, 0, 0, time.UTC)
+
+	fmt.Println("date for today is ", createDate)
+	fmt.Println("formatted date", createDate.Format("Monday 02 Jan 2006"))
+}
