42f9d7e6083fddde18a4a5d64babee773f5887b9
yo-404 <yogeshy2801@gmail.com>
2023-08-28 18:25:46 +0530

    getting one course by id

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//  model for courses - file
+// model for courses - file
 type Course struct {
 	CourseId    string  `json:"courseid`
 	CourseName  string  `json:"coursname"`
@@ -24,25 +26,42 @@ type Author struct {
 var courses []Course
 
 // middleware
-func (c *Course)IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName=""
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
 }
 
 func main() {
 	fmt.Println("Welcome to api creations")
 }
 
-
 // controller - file
 
 // serve home route
 
-func serveHome(w http.ResponseWriter, r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to API creation and handling</h1>"))
 }
 
-func getAllCourses(w http.ResponseWriter, r *http.Request){
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get one course by id")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through the courses , finding matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("no course found with the given ID")
+	return
 }
