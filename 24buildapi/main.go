643350416278ca5d57ede981748166ebf0dee0d4
yo-404 <yogeshy2801@gmail.com>
2023-08-28 15:23:47 +0530

    api response json format

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 //  model for courses - file
 type Course struct {
@@ -26,4 +30,19 @@ func (c *Course)IsEmpty() bool {
 
 func main() {
 	fmt.Println("Welcome to api creations")
+}
+
+
+// controller - file
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to API creation and handling</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
 }
