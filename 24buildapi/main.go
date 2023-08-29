f1971a00f2343b981ca0f47418af0c4064f5e3d9
yo-404 <yogeshy2801@gmail.com>
2023-08-29 15:12:18 +0530

    create new course with unique course ID

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -27,7 +30,8 @@ var courses []Course
 
 // middleware
 func (c *Course) IsEmpty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseId == ""
 }
 
 func main() {
@@ -63,5 +67,32 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode("no course found with the given ID")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if : body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("body is empty . No data was present !!")
+	}
+
+	// what if data is - {}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data was present in json !!")
+		return
+	}
+
+	// generate a unique id , convert into string
+	// add / append new course into course db/slice in our case
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
 	return
 }
