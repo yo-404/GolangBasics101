22fe776ed01d89954198dc9bafcafb4e9f178d1e
yo-404 <yogeshy2801@gmail.com>
2023-10-12 22:51:18 +0530

    handlefunc handleadd added

--- 33commandlineapp/videos/main.go
+++ 33commandlineapp/videos/main.go
@@ -32,4 +32,78 @@ func main() {
 		os.Exit(1)
 	}
 
+	switch os.Args[1] {
+	case "get": //if its the 'get' command
+		HandleGet(getCmd, getAll, getID)
+	case "add":
+		HandleAdd(addCmd, addID, addTitle, addUrl, addImageUrl, addDesc)
+	default:
+	}
+
+}
+
+func HandleGet(getCmd *flag.FlagSet, all *bool, id *string) {
+
+	getCmd.Parse(os.Args[2:])
+
+	if *all == false && *id == "" {
+		fmt.Print("id is required or specify --all for all videos")
+		getCmd.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *all {
+		//return all videos
+		videos := getVideos()
+
+		fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
+		for _, video := range videos {
+			fmt.Printf("%v \t %v \t %v \t %v \t %v \n", video.Id, video.Title, video.Url, video.Imageurl, video.Description)
+		}
+
+		return
+	}
+
+	if *id != "" {
+		videos := getVideos()
+		id := *id
+		for _, video := range videos {
+			if id == video.Id {
+				fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
+				fmt.Printf("%v \t %v \t %v \t %v \t %v \n", video.Id, video.Title, video.Url, video.Imageurl, video.Description)
+			}
+		}
+	}
+
+}
+
+func ValidateVideo(addCmd *flag.FlagSet, id *string, title *string, url *string, imageUrl *string, description *string) {
+
+	addCmd.Parse(os.Args[2:])
+
+	if *id == "" || *title == "" || *url == "" || *imageUrl == "" || *description == "" {
+		fmt.Print("all fields are required for adding a video")
+		addCmd.PrintDefaults()
+		os.Exit(1)
+	}
+
+}
+
+func HandleAdd(addCmd *flag.FlagSet, id *string, title *string, url *string, imageUrl *string, description *string) {
+
+	ValidateVideo(addCmd, id, title, url, imageUrl, description)
+
+	video := video{
+		Id:          *id,
+		Title:       *title,
+		Description: *description,
+		Imageurl:    *imageUrl,
+		Url:         *url,
+	}
+
+	videos := getVideos()
+	videos = append(videos, video)
+
+	saveVideos(videos)
+
 }
